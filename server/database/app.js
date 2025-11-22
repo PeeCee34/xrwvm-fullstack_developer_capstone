77668b3689343d978810bb958500a0147f1c8e0c
PeeCee34 <porelivingstone22@gmail.com>
2025-11-21 20:31:54 -0500

    Fix dealer endpoints: corrected model name and JSON path

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,40 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+  try {
+    const dealers = await Dealerships.find({});
+    res.json(dealers);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try {
+    const state = req.params.state;
+    const dealers = await Dealerships.find({ state: state });
+    res.json(dealers);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+  try {
+    const dealerId = req.params.id;
+    const dealers = await Dealerships.findById(dealerId);
+    if (dealer) {
+      res.json(dealer);
+    } else {
+      res.status(404).json({ error: "Dealer not found" });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 //Express route to insert review
