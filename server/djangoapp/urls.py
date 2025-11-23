ea8ed93ca33d22aabbb7c22cc369a8571d288f83
PeeCee34 <porelivingstone22@gmail.com>
2025-11-23 11:40:06 -0500

    Initial commit: fullstack developer capstone project

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='login', view=views.login_user, name='login'),
+    path('register/', views.registration_view, name='register'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
