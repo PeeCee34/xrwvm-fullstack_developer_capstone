bc6e2e50fea4475e36f8df46b0c38bf979218bd0
Pore Camara <118386873+PeeCee34@users.noreply.github.com>
2025-12-02 16:34:17 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-'https://porelivingst-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-'porelivingst-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+                 'https://porelivingst-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
+                 'porelivingst-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
-CSRF_TRUSTED_ORIGINS = [
-    'https://porelivingst-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://porelivingst-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+CSRF_TRUSTED_ORIGINS = ['https://porelivingst-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                        'https://porelivingst-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -67,10 +66,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [ 
-            os.path.join(BASE_DIR, 'frontend/static'),
-            os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
-      
+                  os.path.join(BASE_DIR, 'frontend/static'),
+                  os.path.join(BASE_DIR, 'frontend/build'),
+                  os.path.join(BASE_DIR, 'frontend/build/static'),      
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -102,14 +100,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME':
         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
+    
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
+    
     {
         'NAME':
         'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
+    
     {
         'NAME':
         'django.contrib.auth.password_validation.NumericPasswordValidator',
@@ -150,4 +151,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
