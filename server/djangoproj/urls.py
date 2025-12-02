f6979765d0ff3c9590eb417514f973f8aa68e4de
Pore Camara <118386873+PeeCee34@users.noreply.github.com>
2025-12-02 16:36:25 -0500

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,13 +19,13 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('postreview/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>/',TemplateView.as_view
+         (template_name="index.html")),
+    path('dealer/<int:dealer_id>/',TemplateView.as_view
+         (template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
