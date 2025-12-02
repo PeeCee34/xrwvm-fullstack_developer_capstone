ca3187caf9bdeeb1dc0d8e7ec9e94f34d9af3860
Pore Camara <118386873+PeeCee34@users.noreply.github.com>
2025-12-02 18:06:26 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,11 +7,17 @@ app_name = "djangoapp"
 
 urlpatterns = [
     path("get_dealers/", views.get_dealerships, name="get_dealers"),
-    path("get_dealers/<str:state>/", views.get_dealerships, name="get_dealers_by_state"),
+    path("get_dealers/<str:state>/",
+         views.get_dealerships,
+         name="get_dealers_by_state"),
     path("login/", views.login_user, name="login"),
     path("register/", views.registration_view, name="register"),
-    path("dealer/<int:dealer_id>/", views.get_dealer_details, name="dealer_details"),
-    path("reviews/dealer/<int:dealer_id>/", views.get_dealer_reviews, name="dealer_reviews"),
+    path("dealer/<int:dealer_id>/",
+         views.get_dealer_details,
+         name="dealer_details"),
+    path("reviews/dealer/<int:dealer_id>/",
+         views.get_dealer_reviews,
+         name="dealer_reviews"),
     path("add_review/", views.add_review, name="add_review"),
     path("get_cars/", views.get_cars, name="get_cars"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
